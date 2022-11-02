235b597dc175cb347808ed9374e8cff62a020670
Joel Choi <116723335+ChoiVision@users.noreply.github.com>
2022-11-02 10:29:04 +0900

    Update conv.py

--- utils/conv.py
+++ utils/conv.py
@@ -12,22 +12,25 @@ class ConvBn(nn.Module):
 
     def forward(self, x):
         return self.conv_bn(x)
-
+    
 class ConvBnAct(nn.Module):
-    def __init__(self, in_c, out_c, k, s, act= None, p= 'same', bias= False, **kwargs):
+    def __init__(self, in_c, out_c, k, s, act= None, p= 1, bias= False, **kwargs):
         super().__init__()
 
         if act is None:
             act= nn.ReLU(inplace= True)
 
-        self.conv_bn_act= nn.Sequential(
-            nn.Conv2d(in_channels= in_c, out_channels= out_c, kernel_size= k, stride= s, padding= p, bias= bias),
-            nn.BatchNorm2d(out_c),
-            act
-        )
+        self.act= act
+
+        self.conv= nn.Conv2d(in_channels= in_c, out_channels= out_c, kernel_size= k, stride= s, padding= p, bias= bias),
+        self.bn= nn.BatchNorm2d(out_c)
 
     def forward(self, x):
-        return self.conv_bn_act(x)
+        x= self.conv(x)
+        x= self.bn(x)
+        x= self.act(x)
+
+        return x
 
 
 class BnActConv(nn.Module):
