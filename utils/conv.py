65b789f07637bd86d533062a5ab9f37ba93af9c7
Joel Choi <116723335+ChoiVision@users.noreply.github.com>
2022-11-02 23:11:33 +0900

    auto_pad

--- utils/conv.py
+++ utils/conv.py
@@ -1,12 +1,21 @@
 import torch
 import torch.nn as nn
 
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    # Pad to 'same' shape outputs
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
 class ConvBn(nn.Module):
-    def __init__(self, in_c, out_c, k, s, p= 'same', bias= False, **kwargs):
+    def __init__(self, in_c, out_c, k, s, d= 1, p= None, bias= False, **kwargs):
         super().__init__()
         
         self.conv_bn= nn.Sequential(
-            nn.Conv2d(in_channels= in_c, out_channels= out_c, kernel_size= k, stride= s, padding= p, bias= bias),
+            nn.Conv2d(in_channels= in_c, out_channels= out_c, kernel_size= k,
+             stride= s, padding= autopad(k, s, d), bias= bias),
             nn.BatchNorm2d(out_c),
         )
 
@@ -14,16 +23,18 @@ class ConvBn(nn.Module):
         return self.conv_bn(x)
     
 class ConvBnAct(nn.Module):
-    def __init__(self, in_c, out_c, k, s, act= None, p= 1, bias= False, **kwargs):
+    def __init__(self, in_c, out_c, k, s, d= 1, act= None, p= None, bias= False, **kwargs):
         super().__init__()
 
         if act is None:
             act= nn.ReLU(inplace= True)
 
-        self.act= act
-
-        self.conv= nn.Conv2d(in_channels= in_c, out_channels= out_c, kernel_size= k, stride= s, padding= p, bias= bias),
-        self.bn= nn.BatchNorm2d(out_c)
+        self.conv_bn_act= nn.Sequential(
+            nn.Conv2d(in_channels= in_c, out_channels= out_c, kernel_size= k, stride= s, 
+            padding= autopad(k, s, d), bias= bias),
+            nn.BatchNorm2d(out_c),
+            act
+        )
 
     def forward(self, x):
         x= self.conv(x)
@@ -34,12 +45,13 @@ class ConvBnAct(nn.Module):
 
 
 class BnActConv(nn.Module):
-    def __init__(self, in_c, out_c, k, s, p= 'same', b= False, **kwargs):
+    def __init__(self, in_c, out_c, k, s, d=1, p= None, b= False, **kwargs):
         super().__init__()
         self.block= nn.Sequential(
             nn.BatchNorm2d(in_c),
-            nn.ReLU(True),
-            nn.Conv2d(in_channels= in_c, out_channels=out_c, kernel_size= k, stride= s, padding= p, bias= b, **kwargs)
+            nn.ReLU(inplace= True),
+            nn.Conv2d(in_channels= in_c, out_channels=out_c, kernel_size= k, stride= s, 
+            padding= autopad(k, s, d), bias= b, **kwargs)
         )
 
     def forward(self, x):
